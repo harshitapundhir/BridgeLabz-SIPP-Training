604d84d2e39c06bab9ded0bcf8e2a7da660b07a5
harshitapundhir <harshita.pundhir_cs22@gla.ac.in>
2025-06-14 02:48:35 +0530

    adding solution of factorial

--- practice-bridge-labz/day3/level 1/factorialusingloop.java
+++ practice-bridge-labz/day3/level 1/factorialusingloop.java
@@ -1,16 +1,14 @@
-import java.util.Scanner;
-public class factorial {
+ import java.util.Scanner;
+public class factorialusingloop {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number ");
         int n = sc.nextInt();
-        long fact = 1;
-        int i = 1;
-        while (i<=n){
+        int fact = 1;
+        for(int i=1; i<=n; i++ ){
             fact *=i;
-            i++;
         }
+       
         System.out.println("the factorial of " + n + " is " +fact);
     }
-    
 }
