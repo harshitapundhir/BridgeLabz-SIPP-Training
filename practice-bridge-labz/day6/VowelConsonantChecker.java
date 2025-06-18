c263d08aa370905ceb88cc86dd2a40eafdcaf85b
harshitapundhir <harshita.pundhir_cs22@gla.ac.in>
2025-06-18 10:59:32 +0530

    added solutions of the programs strings

--- practice-bridge-labz/day6/VowelConsonantChecker.java
+++ practice-bridge-labz/day6/VowelConsonantChecker.java
@@ -1,52 +1,35 @@
 import java.util.Scanner;
 
 public class VowelConsonantChecker {
-
-    public static String getCharType(char ch) {
-        if (ch >= 'A' && ch <= 'Z') {
-            ch = (char) (ch + 32);
-        }
-        if (ch >= 'a' && ch <= 'z') {
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                return "Vowel";
-            } else {
-                return "Consonant";
-            }
+    public static String checkCharType(char c) {
+        if (c >= 'A' && c <= 'Z') c += 32;
+        if (c >= 'a' && c <= 'z') {
+            if ("aeiou".indexOf(c) >= 0) return "Vowel";
+            return "Consonant";
         }
         return "Not a Letter";
     }
 
-    public static String[][] getCharTypeArray(String text) {
-        int len = 0;
-        try {
-            while (true) {
-                text.charAt(len);
-                len++;
-            }
-        } catch (Exception e) {
-        }
-
-        String[][] result = new String[len][2];
-        for (int i = 0; i < len; i++) {
-            result[i][0] = String.valueOf(text.charAt(i));
-            result[i][1] = getCharType(text.charAt(i));
+    public static String[][] analyzeCharacters(String str) {
+        String[][] result = new String[str.length()][2];
+        for (int i = 0; i < str.length(); i++) {
+            result[i][0] = String.valueOf(str.charAt(i));
+            result[i][1] = checkCharType(str.charAt(i));
         }
         return result;
     }
 
-    public static void displayResult(String[][] arr) {
-        System.out.println("Character\tType");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i][0] + "\t\t" + arr[i][1]);
-        }
+    public static void displayTable(String[][] arr) {
+        System.out.println("Char\tType");
+        for (String[] row : arr)
+            System.out.println(row[0] + "\t" + row[1]);
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String text = sc.nextLine();
-
-        String[][] result = getCharTypeArray(text);
-        displayResult(result);
+        String input = inp.nextLine();
+        String[][] result = analyzeCharacters(input);
+        displayTable(result);
     }
 }
