88878b1a67d33eba0e9fdabd9c5d6790d0a74ba1
harshitapundhir <harshita.pundhir_cs22@gla.ac.in>
2025-06-23 12:28:22 +0530

    folder removed from oops branch

--- core-java/day2/level 2/QuotientRemainder.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class QuotientRemainder {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int a = input.nextInt(), b = input.nextInt();
-        System.out.printf("The Quotient is %d and Reminder is %d of two number %d and %d\n", a / b, a % b, a, b);
-    }
-}
-
