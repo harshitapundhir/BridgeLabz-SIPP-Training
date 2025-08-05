8086eb7b9259724da56f3655d3bc3ad44ecc25b8
harshitapundhir <harshita.pundhir_cs22@gla.ac.in>
2025-08-05 09:06:34 +0530

    adding solutions for exception handling

---
+++ generics and collections/Exceptions/AgeValidators.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
+public class AgeValidators {
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        try {
+            validateAge(age);
+            System.out.println("Access granted!");
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ generics and collections/Exceptions/BankAccount.java
@@ -0,0 +1,31 @@
+class InsufficientBalanceException extends Exception {
+    InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
+public class BankAccount {
+    double balance = 1000;
+
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Invalid amount!");
+        }
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        }
+        balance -= amount;
+        System.out.println("Withdrawal successful, new balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount();
+        try {
+            acc.withdraw(1200);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
